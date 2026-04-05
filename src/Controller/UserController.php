96d04802f7e8c600fa27986bfaa3a03a374ed195
MedAdamDerouich <adamderouiche5@gmail.com>
2026-04-05 19:56:25 +0100

    upgrade in the admin user crud

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,14 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Entity\Wallet;
-use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -26,62 +23,30 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_user_new', methods: ['GET', 'POST'])]
-    public function new(
-        Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordHasher
-    ): Response {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user, ['include_password' => true]);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $plain = $form->get('plainPassword')->getData();
-            $user->setPassword($passwordHasher->hashPassword($user, $plain));
-            // role is already mapped directly via UserType → setRole()
-
-            $em->persist($user);
-            $em->flush();
-
-            // Auto-create wallet
-            $wallet = new Wallet();
-            $wallet->setUser($user);
-            $em->persist($wallet);
-            $em->flush();
-
-            $this->addFlash('success', 'Utilisateur créé avec succès.');
-            return $this->redirectToRoute('app_admin_users');
-        }
-
-        return $this->render('admin/users/form.html.twig', [
-            'userForm' => $form->createView(),
-            'user'     => $user,
+    #[Route('/{id}', name: 'app_admin_user_show', methods: ['GET'])]
+    public function show(User $user): Response
+    {
+        return $this->render('admin/users/show.html.twig', [
+            'user' => $user,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_user_edit', methods: ['GET', 'POST'])]
-    public function edit(
+    #[Route('/{id}/delete', name: 'app_admin_user_delete', methods: ['POST'])]
+    public function delete(
         User $user,
         Request $request,
-        EntityManagerInterface $em,
-        UserPasswordHasherInterface $passwordHasher
+        EntityManagerInterface $em
     ): Response {
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            // role is already mapped directly via UserType → setRole()
-            $em->flush();
-
-            $this->addFlash('success', 'Utilisateur modifié avec succès.');
+        if (!$this->isCsrfTokenValid('delete-user-' . $user->getId(), $request->request->get('_token'))) {
+            $this->addFlash('error', 'Token CSRF invalide.');
             return $this->redirectToRoute('app_admin_users');
         }
 
-        return $this->render('admin/users/form.html.twig', [
-            'userForm' => $form->createView(),
-            'user'     => $user,
-        ]);
+        $em->remove($user);
+        $em->flush();
+
+        $this->addFlash('success', 'Utilisateur supprimé avec succès.');
+        return $this->redirectToRoute('app_admin_users');
     }
 
     #[Route('/{id}/toggle', name: 'app_admin_user_toggle', methods: ['POST'])]
